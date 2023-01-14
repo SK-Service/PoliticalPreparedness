0aeb73512262f806f8d49feefcc07392b7e6bbb3
Sunil Kr. Singh <sunil.singh@economical.com>
2023-01-14 10:41:26 -0500

    compilation challenge with Fused Location Client Last Location

--- starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
@@ -1,12 +1,19 @@
 package com.example.android.politicalpreparedness.representative
 
+import android.Manifest
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -17,12 +24,18 @@ import com.example.android.politicalpreparedness.databinding.FragmentRepresentat
 import com.example.android.politicalpreparedness.election.TAG
 import com.example.android.politicalpreparedness.network.models.Address
 import com.google.android.gms.location.LocationServices
+import android.provider.Settings
+import com.google.android.gms.location.FusedLocationProviderClient
+import java.util.*
 
 
 const val TAG_R = "RepresentativeFragment"
 class RepresentativeFragment: Fragment() , AdapterView.OnItemSelectedListener {
     private lateinit var repViewModel: RepresentativeViewModel
     private lateinit var binding: FragmentRepresentativeBinding
+    private val permissionId = 2
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -30,6 +43,9 @@ class RepresentativeFragment: Fragment() , AdapterView.OnItemSelectedListener {
         binding = FragmentRepresentativeBinding.inflate(inflater)
 
         binding.lifecycleOwner = this
+        val mFusedLocationClient = LocationServices.getFusedLocationProviderClient(
+            this.requireActivity())
+
 
         //Setting up the spinner Adapter and binding
         val stateSpinner = binding.state
@@ -122,18 +138,55 @@ class RepresentativeFragment: Fragment() , AdapterView.OnItemSelectedListener {
 
         binding.buttonLocation.setOnClickListener {
             Log.i(TAG_R, "Inside USE MY LOCATION Button click listener")
-            retrieveAddressFromLocationDetection()
+            val address = retrieveAddressFromLocationDetection()
+            binding.repviewmodel.searchMyRepresentative(address.line1 ?: "",
+                                                        address.line2?:"",
+                                                        address.city?: "",
+                                                        address.state?:"",
+                                                        address.zip?:"")
         }
 
         return binding.root
 
     }
 
-    private fun retrieveAddressFromLocationDetection() {
-        val mFusedLocationClient = LocationServices.getFusedLocationProviderClient(
-                                                    this.requireActivity())
+    private fun retrieveAddressFromLocationDetection() : Address {
+        var locationBasedAddress = Address("","","","","")
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if (location != null) {
+                        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+                        val addressList =
+                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
+                        if ((addressList != null && addressList.size > 0)) {
+                            val geoAddress = addressList.get(0)
+                            val line1 = StringBuilder()
+                            for (i in 0 until geoAddress.maxAddressLineIndex) {
+                                line1.append(geoAddress.getAddressLine(i)).append("")
+                            }
+                            locationBasedAddress.line1 = line1.toString()
+                            Log.i(TAG_R,"Address Line 1:${locationBasedAddress.line1}")
+                            Log.i(TAG_R, "LOCALITY: ${geoAddress.locality}")
+                            Log.i(TAG_R, "PostalCode: ${geoAddress.postalCode}")
+                        }
+                    }
+                }
+            }else {
+                Toast.makeText(requireContext(),
+                    "Please turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
 
+        }else {
+            requestPermissions()
+        }
+
+        return locationBasedAddress
     }
+
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
            val stateString = parent?.getItemAtPosition(pos).toString()
@@ -145,13 +198,39 @@ class RepresentativeFragment: Fragment() , AdapterView.OnItemSelectedListener {
 
     }
 
-//    private fun isLocationEnabled(): Boolean {
-//        val locationManager: LocationManager =
-//            getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-//                                        locationManager.isProviderEnabled(
-//            LocationManager.NETWORK_PROVIDER
-//        )
-//    }
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                                        locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            permissionId
+        )
+    }
 
 }
